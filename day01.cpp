049c34d471ed546b34da5507011fb84aa12aa897
smritisharma2924 <smritisharma2905@gmail.com>
2025-08-08 01:11:53 +0530

    Add Day 1 basics

---
+++ day01.cpp
@@ -0,0 +1,101 @@
+// First Code in C++
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout<<"Hello World!";
+// };
+
+
+
+// Going to next line while printing
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout<<"Hello World!"<<endl<<"Hello Coders!";
+// };
+
+
+
+// Variables and Data Types
+
+// int : 1, 2, 3, 4
+// float : 1.23, 6.626
+// double : 1.2345678, 2.6855467
+// char : a, b, c
+// string : how are you, good morning
+// boolean : true(1), false(0)
+
+// syntax for declaring a variable : data_type variable_name ;
+// syntax for declaring and assigning a value to the variable : data_type variable_name = assignment_value ;
+
+
+
+// Using int data type
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     //variable declaration and value assignment
+//     int a=29, b=24 ; 
+//     cout<<a+b;
+// };
+
+
+
+// Using char data type
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     char c = 'r';
+//     cout<<c;
+// };
+
+
+
+// Using float data type
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     float d = 29.24;
+//     cout<<d;
+// };
+
+
+
+// Using double data type
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     double e = 2924.22;
+//     cout<<e;
+// };
+
+
+
+// Using boolean data type
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     bool f = true;
+//     cout<<f;
+// };
