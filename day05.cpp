78da4a5daace3877cd12b1fdab4bf7bf81996442
smritisharma2924 <smritisharma2905@gmail.com>
2025-11-20 21:02:50 +0530

    Add Day 5 basics

--- day05.cpp
+++ day05.cpp
@@ -154,14 +154,14 @@
 // 5 4 3 2
 // 5 4 3 2 1
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main() {
-    for(int i=5 ; i>=1 ; i--) {
-        for(int j=5 ; j>=i ; j--) {
-            cout<<j<<" ";
-        }
-        cout<<endl;
-    }
-}
+// int main() {
+//     for(int i=5 ; i>=1 ; i--) {
+//         for(int j=5 ; j>=i ; j--) {
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
