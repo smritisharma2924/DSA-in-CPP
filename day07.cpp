aa9891b5fa27226c8bf67c4aa8f3def9525922ee
smritisharma2924 <smritisharma2905@gmail.com>
2025-11-22 15:58:16 +0530

    Add Day 7 basics

--- day07.cpp
+++ day07.cpp
@@ -154,29 +154,29 @@
 //   * *
 //    *
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main() {
-    int n;
-    cout<<"Enter number of rows : ";
-    cin>>n;
-    for(int i=1 ; i<=n ; i++) {
-        for(int j=n-i+1 ; j>=1 ; j--) {
-            cout<<" ";
-        }
-        for(int k=1 ; k<=i ; k++) {
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-    for(int i=n ; i>=1 ; i--) {
-        for(int j=n-i+1 ; j>=1 ; j--) {
-            cout<<" ";
-        }
-        for(int k=1 ; k<=i ; k++) {
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-}
+// int main() {
+//     int n;
+//     cout<<"Enter number of rows : ";
+//     cin>>n;
+//     for(int i=1 ; i<=n ; i++) {
+//         for(int j=n-i+1 ; j>=1 ; j--) {
+//             cout<<" ";
+//         }
+//         for(int k=1 ; k<=i ; k++) {
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     for(int i=n ; i>=1 ; i--) {
+//         for(int j=n-i+1 ; j>=1 ; j--) {
+//             cout<<" ";
+//         }
+//         for(int k=1 ; k<=i ; k++) {
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
