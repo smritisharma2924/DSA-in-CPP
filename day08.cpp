3cc6601df2eb49ae1b7c1c2a56782706dd6269bd
smritisharma2924 <smritisharma2905@gmail.com>
2025-11-23 00:13:49 +0530

    Add Day 8 Basics

---
+++ day08.cpp
@@ -0,0 +1,49 @@
+// Operators
+
+// 1. Arithmetic operators
+//     - Unary (one operand) ex : ++ -- 
+//     - Binary (two operands) ex : + - * / %
+
+//     Precendence Order - (associativity : left to right)
+//     {* , / , %} > {+ , -}
+
+// 2. Comparison operators
+//     they give answer either true or false i.e. 0 or 1
+//     associativity : left to right in any expression
+//     ex : == , > , < , >= , <= , !=
+
+// 3. Logical operators
+//     they give answer either true or false i.e. 0 or 1
+//     ex : && , || , !
+
+
+
+// Example
+// To check if the enetered letter is a vowel or not.
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     char name;
+//     cout<<"Enter a letter : ";
+//     cin>>name;
+//     if(name=='a' || name=='e' || name=='i' || name=='o' || name=='u') {
+//         cout<<"Is a vowel.";
+//     }
+//     else {
+//         cout<<"Not a vowel.";
+//     }
+// }
+
+
+
+// 4. Bitwise operators
+//     it first converts the operand into binary and then performs operation.
+//     ex : & , | , ^ , ~ , << , >>
+
+//     Precendence Order - (associativity : left to right)
+//     {<< , >>} > {&, | , ~}
+
+// 5. Assignment operator 
+//     ex : = , += , -= , *= , %=
