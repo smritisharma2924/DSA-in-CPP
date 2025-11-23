ed6e0f41b5099ee5e5c5e736549e6c13d52d58d4
smritisharma2924 <smritisharma2905@gmail.com>
2025-11-23 15:49:11 +0530

    Add Day 10 basics

---
+++ day10.cpp
@@ -0,0 +1,40 @@
+// Number System Conversions
+
+//Decimal to Binary conversion
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int num;
+//     cout<<"Enter decimal number : ";
+//     cin>>num;
+//     int bin=0,rem=0,mul=1;
+//     while (num>0) {
+//         rem = num%2;
+//         bin += rem*mul;
+//         num/=2;
+//         mul *= 10;
+//     }
+//     cout<<"Binary equivalent is : "<<bin;
+// }
+
+
+
+// Binary to Decimal conversion
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int bin;
+    cout<<"Enter binary number : ";
+    cin>>bin;
+    int dec=0,rem=0,mul=1;
+    while (bin>0) {
+        rem = bin%10;
+        bin /= 10;
+        dec += rem*mul;
+        mul *= 2;
+    }
+    cout<<"Decimal equivalent is : "<<dec;
+}
