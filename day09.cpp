32ca04f3c6d850dc0d1d04b222e74dc7cc4f4873
smritisharma2924 <smritisharma2905@gmail.com>
2025-11-23 11:41:33 +0530

    Add Day 9 basics

---
+++ day09.cpp
@@ -0,0 +1,188 @@
+// While Loop
+// Syntax :
+//     intitialization
+//     while (condition)
+//     {
+//         /* code */
+//     }
+
+
+
+// Example 1
+// Print numbers from 1 to 10
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int i=1;
+//     while(i<=10) {
+//         cout<<i<<" ";
+//         i++;
+//     }
+// }
+
+
+
+// Example 2
+// Multiplication Table Program
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout<<"Enter number : ";
+//     cin>>n;
+//     int i=1;
+//     while(i<=10) {
+//         cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+//         i++;
+//     }
+// }
+
+
+
+// Example 3
+// Find factorial of a number
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     int i=1,fact=1;
+//     cout<<"Enter number : ";
+//     cin>>n;
+//     while(i<=n) {
+//         fact *= i;
+//         i++;
+//     }
+//     cout<<"Factorial is : "<<fact;
+// }
+
+
+
+// Do While loop
+// Syntax :
+//     initialization
+//     do
+//     {
+//         /* code */
+//     } while (condition);
+    
+
+
+// Example 1
+// Sum of n natural numbers
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int i=1, n, sum=0;
+//     cout<<"Enter number : ";
+//     cin>>n;
+//     do {
+//         sum+=i;
+//         i++;
+//     } while(i<=n);
+//     cout<<"Sum is : "<<sum;
+// }
+
+
+
+// Break and Continue statements
+
+// break Statement : stops the loop immediately and exits from it.
+// continue Statement : skips the current iteration and moves to the next one.
+
+
+
+// Example 1
+// Print numbers from 1 to 10 and stop when the number entered by the user is about to print.
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout<<"Enter number : ";
+//     cin>>n;
+//     int i=1;
+//     do {
+//         cout<<i<<" ";
+//         i++;
+//         if(i==n) {
+//             break;
+//         }
+//     } while(i<=10);
+// }
+
+
+
+// Example 2
+// Print numbers from 1 to 10 and skip the number which is entered by the user.
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout<<"Enter number : ";
+//     cin>>n;
+//     int i=1;
+//     do {
+//         if(i==n) {
+//             i++;
+//             continue;
+//         }
+//         cout<<i<<" ";
+//         i++;
+//     } while(i<=10);
+// }
+
+
+
+// Switch Statement
+// used when you want to compare a variable against multiple constant values and execute different code depending on which case matches.
+// It’s a cleaner alternative to using multiple if…else if conditions.
+
+// Syntax :
+//     switch(expression) {
+//     case constant1:
+//         // code
+//         break;
+//     case constant2:
+//         // code
+//         break;
+//     default:
+//         // code if no case matches
+// }
+
+
+
+// Example 1
+// Print day name based on the entered day number by the user.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day;
+    cout << "Enter day number (1-7): ";
+    cin >> day;
+
+    switch(day) {
+        case 1: cout << "Monday"; break;
+        case 2: cout << "Tuesday"; break;
+        case 3: cout << "Wednesday"; break;
+        case 4: cout << "Thursday"; break;
+        case 5: cout << "Friday"; break;
+        case 6: cout << "Saturday"; break;
+        case 7: cout << "Sunday"; break;
+
+        default:
+            cout << "Invalid day!";
+    }
+}
