e79e87e4c44b6ad2b1beb6e3ea7c6d849a9c578c
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-01 23:31:41 +0530

    Add Day 40 basics

--- day40.cpp
+++ day40.cpp
@@ -46,3 +46,15 @@
 //     }
 //     return 0;
 // }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int arr[5] = {1,2,3,4,5};
+//     int *ptr = arr;
+//     for (int i=0 ; i < 5 ; i++) {
+//         cout<<ptr<<endl;
+//         ptr++;
+//     }
+//     return 0;
+// }
