eecbc1e5f2171ca3d4039171576da23e4a7e0614
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-02 23:39:45 +0530

    Add Day 41 basics

--- day41.cpp
+++ day41.cpp
@@ -141,3 +141,24 @@
 // &num = address of num
 // ptr  = stores the address
 // *ptr = value present at that address
+
+
+
+// Problem - Double the values of a given array
+// #include<iostream>
+// using namespace std;
+
+// void dob(int* nums) {
+//     for (int i=0 ; i < 5 ; i++) {
+//         *(nums+i) *= 2;
+//     }
+// }
+
+// int main() {
+//     int arr[] = {1,2,3,4,5};
+//     dob(arr);
+//     for (int i=0 ; i < 5 ; i++) {
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+// }
