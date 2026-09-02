524f64331e7a31a54f5c8d2eece8c1202df9cc0b
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-02 23:13:41 +0530

    Add Day 41 basics

---
+++ day41.cpp
@@ -0,0 +1,21 @@
+// Pointers with char array
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     char arr[] = "1 2 3 4";
+//     char *ptr = arr;
+//     cout<<arr<<endl;
+//     cout<<ptr<<endl;
+
+//     int arr1[] = {1, 2, 3, 4};
+//     int *ptr1 = arr1;
+//     cout << arr1 << endl;
+//     cout << ptr1;
+//     return 0;
+// }
+
+
+// int*  with cout -> prints the address
+// char* with cout -> treated as a C-string and prints characters until '\0'
+// The pointer-array relationship itself is the same.
