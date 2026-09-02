8ce246310fa77cd55683811b99323696c58e490b
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-02 23:44:14 +0530

    Add Day 41 basics

--- day41.cpp
+++ day41.cpp
@@ -162,3 +162,28 @@
 //     }
 //     return 0;
 // }
+
+
+
+// Problem - Swap the values
+// #include<iostream>
+// using namespace std;
+
+// void swap(int* p1, int* p2) {
+//     int temp = *p1;
+//     *p1 = *p2;
+//     *p2 = temp;
+// }
+
+// int main() {
+//     int a,b;
+//     cout<<"Enter a: ";
+//     cin>>a;
+//     cout<<"Enter b: ";
+//     cin>>b;
+//     cout<<"Swapping..."<<endl;
+//     swap(&a, &b);
+//     cout<<"a: "<<a<<endl;
+//     cout<<"b: "<<b;
+//     return 0;
+// }
