6f658ce9d883633a60eb3fefe89343762d8ef7fd
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-05 17:14:15 +0530

    Add Day 45 basics

---
+++ day45.cpp
@@ -0,0 +1,21 @@
+// Recursion
+// a function which calls itself again and again
+// until a specifc condition is met
+
+
+// #include<iostream>
+// using namespace std;
+
+// void bdayWish(int n) {
+//     if (n == 0) cout<<"Happy Bdayyy!!!";
+//     else {
+//         cout<<n<<" days left for bday..."<<endl;
+//         bdayWish(n-1);
+//     }
+// }
+
+// int main() {
+//     int n=5;
+//     bdayWish(n);
+//     return 0;
+// }
