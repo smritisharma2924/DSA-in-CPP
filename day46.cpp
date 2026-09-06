ea030de9c0701e9c8ef78d08297bbfe3400f16d9
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-06 18:01:57 +0530

    Add Day 46 basics

--- day46.cpp
+++ day46.cpp
@@ -14,3 +14,22 @@
 //     cout<<"Factorial: "<<fact(num);
 //     return 0;
 // }
+
+
+
+// Problem 2 - Find the sum of first N natural number
+// #include<iostream>
+// using namespace std;
+
+// int sum(int n) {
+//     if (n == 0) return 0;
+//     else return n + sum(n-1);
+// }
+
+// int main() {
+//     int num;
+//     cout<<"Enter num: ";
+//     cin>>num;
+//     cout<<"Sum is: "<<sum(num);
+//     return 0;
+// }
