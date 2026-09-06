51ce5b3c168f906c855ff0812ec58c2eb8e6c0ce
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-06 17:56:50 +0530

    Add Day 46 basics

---
+++ day46.cpp
@@ -0,0 +1,16 @@
+// Problem 1 - Find factorial of a number
+// #include<iostream>
+// using namespace std;
+
+// int fact(int n) {
+//     if (n == 1 || n == 0) return 1;
+//     else return n * fact(n-1);
+// }
+
+// int main() {
+//     int num;
+//     cout<<"Enter num: ";
+//     cin>>num;
+//     cout<<"Factorial: "<<fact(num);
+//     return 0;
+// }
