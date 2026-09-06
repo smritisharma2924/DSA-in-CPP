f7bfd2a6eb3ea6ee8fc8bbfa753adce15432c718
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-06 17:38:40 +0530

    Add Day 45 basics

--- day45.cpp
+++ day45.cpp
@@ -19,3 +19,26 @@
 //     bdayWish(n);
 //     return 0;
 // }
+
+
+
+// Problem 1 - Print from 1 to N
+// #include<iostream>
+// using namespace std;
+
+// void print(int n) {
+//     if (n == 1) {
+//         cout<<1<<" ";
+//         return;
+//     }
+//     print(n-1);
+//     cout<<n<<" ";
+// }
+
+// int main() {
+//     int num;
+//     cout<<"Enter num: ";
+//     cin>>num;
+//     print(num);
+//     return 0;
+// }
