0038a74b6d8757635b9301f0b18f65e660335a63
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-08 18:09:45 +0530

    Add Day 47 basics

--- day47.cpp
+++ day47.cpp
@@ -13,3 +13,21 @@
 //     print(arr, 0, 6);
 //     return 0;
 // }
+
+
+
+// Problem 2 - print array using recursion in reverse
+// #include<iostream>
+// using namespace std;
+
+// void print(int arr[], int index, int len) {
+//     if (index == len) return;
+//     print(arr, index+1, len);
+//     cout<<arr[index]<<" ";
+// }
+
+// int main() {
+//     int arr[] = {1,2,3,4,5,6};
+//     print(arr, 0, 6);
+//     return 0;
+// }
