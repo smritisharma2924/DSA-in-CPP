b9003031f836e7cf775605ddec2bf9d4a9843d1e
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-14 16:48:55 +0530

    Add Day 49 basics

---
+++ day49.cpp
@@ -0,0 +1,16 @@
+// Problem 1 - use linear search recursively
+// #include<iostream>
+// using namespace std;
+
+// bool linearSearch(int arr[], int target, int index, int size) {
+//     if (index == size) return false;
+//     if (arr[index] == target) return true;
+//     else return linearSearch(arr, target, index+1, size);
+// }
+
+// int main() {
+//     int arr[] = {2,3,6,7,1,5};
+//     int target = 7;
+//     cout<<linearSearch(arr, target, 0, 6);
+//     return 0;
+// }
