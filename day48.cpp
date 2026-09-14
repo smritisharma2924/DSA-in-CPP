249aa06ea8c9f917843b92c21cd723ea6bfdb1ee
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-14 15:52:13 +0530

    Add Day 48 basics

--- day48.cpp
+++ day48.cpp
@@ -36,3 +36,29 @@
 //     cout<<"Vowels: "<<countVowel(s, s.size()-1);
 //     return 0;
 // }
+
+
+
+// Problem 3 - Reverse a string
+// #include<iostream>
+// using namespace std;
+
+// void rev(string &s, int start, int end) {
+//     if (start >= end) return;
+//     else {
+//         char temp = s[start];
+//         s[start] = s[end];
+//         s[end] = temp;
+//         rev(s, start+1, end-1);
+//     }
+//     return;
+// }
+
+// int main() {
+//     string s;
+//     cout<<"Enter string: ";
+//     cin>>s;
+//     rev(s, 0, s.size()-1);
+//     cout<<"Reversed: "<<s;
+//     return 0;
+// }
