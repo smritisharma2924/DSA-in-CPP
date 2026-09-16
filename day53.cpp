4cdc47f1ba4f2e639bf0c45460ebb2908c318790
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-16 22:15:15 +0530

    Add Day 53 basics

--- day53.cpp
+++ day53.cpp
@@ -30,3 +30,33 @@
 //     }
 //     return 0;
 // }
+
+
+
+// Print subsequences for a string
+
+// #include<iostream>
+// using namespace std;
+
+// void subset(string s, int idx, int n, vector<string>& ans, string temp) {
+//     if (idx == n) {
+//         ans.push_back(temp);
+//         return;
+//     }
+//     // not included
+//     subset(s, idx+1, n, ans, temp);
+//     // included
+//     temp.push_back(s[idx]);
+//     subset(s, idx+1, n, ans, temp);
+// }
+
+// int main() {
+//     string s = "abc";
+//     vector<string>ans;
+//     string temp;
+//     subset(s, 0, s.size(), ans, temp);
+//     for (int i = 0; i < ans.size(); i++) {
+//         cout << "\"" << ans[i] << "\" ";
+//     }
+//     return 0;
+// }
