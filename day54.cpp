8aa6bfaebf31bc4bb5d6d7f99b145f16c92ac61f
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-17 14:02:01 +0530

    Add Day 54 basics

---
+++ day54.cpp
@@ -0,0 +1,27 @@
+// Problem 1 - calculate the sum of all subsets of an array
+// #include<iostream>
+// using namespace std;
+
+// void subsetSum(int arr[], int idx, int n, int sum, vector<int>& ans) {
+//     if (idx == n) {
+//         ans.push_back(sum);
+//         return;
+//     }
+//     // dont include current element in current subset
+//     subsetSum(arr, idx+1, n, sum, ans);
+//     // include current element in current subset
+//     subsetSum(arr, idx+1, n, sum + arr[idx], ans);
+// }
+
+// int main() {
+//     int arr[] = {1,2,3,4};
+//     vector<int> ans;
+//     subsetSum(arr, 0, 4, 0, ans);
+//     int total_sum = 0;
+//     for (int i=0 ; i < ans.size() ; i++) {
+//         cout<<"\""<<ans[i]<<"\""<<" ";
+//         total_sum += ans[i];
+//     }
+//     cout<<endl<<"Total Sum: "<<total_sum;
+//     return 0;
+// }
