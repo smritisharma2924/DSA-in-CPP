e705ff125f70260fc2d65c8c484d8038fa3bc8ab
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-19 11:52:58 +0530

    Add Day 55 basics

--- day55.cpp
+++ day55.cpp
@@ -18,3 +18,25 @@
 //     cout<<ways(arr, 0, 0, 4, 8);
 //     return 0;
 // }
+
+
+
+// Problem 2 - Target sum repetition
+// #include<iostream>
+// using namespace std;
+
+// int subsum(int arr[], int current, int idx, int n, int target) {
+//     if (idx == n) return current==target;
+//     if (current > target) return 0;
+//     // dont use this element
+//     int exclude = subsum(arr, current, idx+1, n, target);
+//     // use this element, and rep is allowed so idx remains the same
+//     int include = subsum(arr, current+arr[idx], idx, n, target);
+//     return exclude+include;
+// }
+
+// int main() {
+//     int arr[] = {2,3,4};
+//     cout<<subsum(arr, 0, 0, 3, 6);
+//     return 0;
+// }
