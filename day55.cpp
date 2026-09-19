b42a26100b4ba33c31da597e85c82a863efb80e3
Smriti Sharma <smritisharma2905@gmail.com>
2026-09-19 11:00:39 +0530

    Add Day 55 basics

--- day55.cpp
+++ day55.cpp
@@ -1,20 +1,20 @@
 // Problem 1 - perfect sum
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int ways(int arr[], int current, int idx, int n, int target) {
-    if (idx == n) {
-        return current == target;
-    }
-    // inlcude current element
-    int include = ways(arr, current+arr[idx], idx+1, n, target);
-    // dont include current element
-    int exclude = ways(arr, current, idx+1, n, target);
-    return include+exclude;
-}
+// int ways(int arr[], int current, int idx, int n, int target) {
+//     if (idx == n) {
+//         return current == target;
+//     }
+//     // inlcude current element
+//     int include = ways(arr, current+arr[idx], idx+1, n, target);
+//     // dont include current element
+//     int exclude = ways(arr, current, idx+1, n, target);
+//     return include+exclude;
+// }
 
-int main() {
-    int arr[] = {2,5,6,1};
-    cout<<ways(arr, 0, 0, 4, 8);
-    return 0;
-}
+// int main() {
+//     int arr[] = {2,5,6,1};
+//     cout<<ways(arr, 0, 0, 4, 8);
+//     return 0;
+// }
